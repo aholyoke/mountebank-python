77e493a625fb0127795f8ed3ed58fe5b6756f60e
Alex Holyoke <alex.holyoke@adroll.com>
2015-03-06 15:45:02 -0800

    Restructured for PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'mountebank-python',
+    packages = ['mountebank-python'],
+    version = '0.1.0',
+    description = 'A thin convenience wrapper for interacting with Mountebank from Python',
+    author = 'Alex Holyoke',
+    author_email = 'aholyoke@uwaterloo.ca',
+    url = 'https://github.com/aholyoke/mountebank-python',
+    download_url = 'https://github.com/aholyoke/mountebank-python/tarball/0.1',
+    keywords = ['testing', 'mountebank'],
+    classifiers = [],
+)
