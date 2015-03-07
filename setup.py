bd019fab310076d09ccaf8da470038fa1fa07473
Alex Holyoke <alex.holyoke@adroll.com>
2015-03-06 17:22:23 -0800

    Renamed mountebank-python folder

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
     name = 'mountebank-python',
     packages = ['mountebank-python'],
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'A thin convenience wrapper for interacting with Mountebank from Python',
     author = 'Alex Holyoke',
     author_email = 'aholyoke@uwaterloo.ca',
+    license = "MIT",
     url = 'https://github.com/aholyoke/mountebank-python',
     download_url = 'https://github.com/aholyoke/mountebank-python/tarball/0.1',
     keywords = ['testing', 'mountebank'],
     classifiers = [],
-    install_requires = [
-    'requests>=2.5.3',
-    ]
+    install_requires = ['requests>=2.5.3'],
 )
